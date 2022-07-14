d74f3a5cf1e6ea3972e10faff191fdff660ab05c
vudaian <mr.vuan24@gmail.com>
2022-07-14 09:43:51 +0700

    change 14-07-2022

--- main.js
+++ main.js
@@ -345,10 +345,28 @@
 
 //Bài 18: Object prototype
 /**
- * 
+    // Làm bài tại đây
+    // function Student(firstName, lastName) {
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    //     this.getFullName = function() {
+    //         return `${this.firstName} ${this.lastName}`;
+    //     }
+    // }
+    // Student.prototype.getFullName = function() {
+    //         return `${this.firstName} ${this.lastName}`;
+    // };
+    // // Ví dụ khi sử dụng
+    // var student = new Student('Long', 'Bui');
+    // console.log(student.firstName);  // 'Long'
+    // console.log(student.lastName);  // 'Bui'
+    // console.log(student.getFullName());  // 'Long Bui'
  */
 
-//Bài 19:
+//Bài 19: Đối tượng date
+/**
+ * 
+ */
 
 
 //Bài 20:
