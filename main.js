93236050de7965f9d7d189408eb0b171420d198c
vudaian <mr.vuan24@gmail.com>
2022-07-15 21:40:20 +0700

    change 15-07-2022

--- main.js
+++ main.js
@@ -417,3 +417,23 @@
  */
 
 //Bài 22: Switch
+/**
+    var date = 2;
+    switch(date) {
+        case 2:
+        case 3:
+        case 4:
+            console.log('Hôm nay là thứ 2,3,4');
+            break;
+        case 5:
+            console.log('Hôm nay là thứ 5');
+            break;
+        default:
+            console.log('Không biết');
+    }
+ */
+
+//Bài 23: Toán tử 3 ngôi
+/**
+ * 
+ */
